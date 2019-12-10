6e90a10cfa1693437f7034e99191c8e4e89df550
quetzal1234 <57687208+quetzal1234@users.noreply.github.com>
2019-12-10 15:22:07 -0600

    Update lbohman2_finalv2.py

--- lbohman2_finalv2.py
+++ lbohman2_finalv2.py
@@ -4,12 +4,15 @@ print("This program creates a text file that can be copy and pasted to compass."
 print("You will be prompted to open a .csv. Your csv must contain the following columns in the following order:")
 print("1. First Name, 2. Last Name, 3. netid, 4. email, 5. linkedin, 6. group")
 print("Please use the same column headers (without the numbers).")
+#prompts user to choose file to open
 from tkinter.filedialog import askopenfilename
 filename = askopenfilename()
 
+#opens infiles and outfiles
 infile = open(filename, "r", encoding="utf-8")
 outfile = open("roster.txt", "w", encoding="utf-8")
 
+#create dictionary accumulator
 csvin = csv.reader(infile)
 headers = next(csvin)
 groups = dict()
@@ -21,6 +24,7 @@ for line in csvin:
     else:
         groups[group].append(student)
 
+#function to deal students who don't supply linkedin links        
 def missinglink(linkedin,firstname,lastname):
     if linkedin == "":
         linkedinline = " "
@@ -28,8 +32,10 @@ def missinglink(linkedin,firstname,lastname):
         linkedinline = '<a href="'+linkedin+'"><img src="http://i.imgur.com/Z9pDAGI.png" alt="Linkedin profile: '+firstname+" "+lastname+'" height="15px" width="15px" margin="1px" /></a> '
     return linkedinline
 
+#loop that creates actual html
 for key in groups:
     students = groups[key]
+    #group html
     groupline = "</p><h2>Group "+key+"</h2> \n<p>"
     outfile.write(groupline)
     for student in students:
@@ -39,6 +45,7 @@ for key in groups:
         linkedin = student[4]
         nameline = firstname+" "+lastname+" "
         emailline = "<a href = "+"'mailto:"+email+"'>"+email+"</a>, \n"
+        #student html chunk
         studenthtml = nameline+missinglink(linkedin,firstname,lastname)+emailline
         outfile.write(studenthtml)
 outfile.close()
